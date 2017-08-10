e2d66ab629ef4afd1b4300544532e97f1bb2cce0
NguyeLongT <nguyelongt@gmail.com>
2017-08-10 16:28:11 -0400

    Saving Changes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$().ready(function () {
+  $('nav').append(
+    "<ul>"
+          +"<li title='PRS'><a href='index.html'>PRS<a/></li>"
+          +"<li title='Home page'><a href='index.html'>Home<a/></li>"
+          +"<li title='About page'><a href='index.html'>About<a/></li>"
+          + "</u>"
+  );
+});
